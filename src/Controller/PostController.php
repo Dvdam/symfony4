ad8a92c580fbb5e83b76d2386f1514c90262b9bd
Dvdam <rdvdam@gmail.com>
2019-06-17 00:13:32 -0300

    doctrine

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,16 +1,61 @@
 <?php
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
-class PostController
+class PostController extends AbstractController
 {
     /**
-     * @Route()
+     * @Route("/", name="app_homepage")
      */
     public function home()
     {
-        return new  Response('Hello tech yogi friends');
+        // return new  Response('Hello tech yogi friends');
+        return $this->render('posts/home.html.twig');
+    }
+
+    // @Route("/blog/how-to-carry-your-sup-with-style-in-ibiza-waters") --- No va
+    
+    /**
+     * @Route("/blog/{postName}", name="post_show")
+     */
+    // public function showPost($post)
+
+    // AHora hacemos uso de las Entidades creadas
+    public function showPost($postName, EntityManagerInterface $em)
+    {
+        // return new Response('future page to show a post content');
+        // return new Response(sprintf('future page to show: "%s"', $post));
+        $repository = $em->getRepository(Post::class);
+
+        // Nuestro objeto repository sabe todo de como consultar en nuestra base de datos y/o tabla
+
+        /** @var Post $post */
+        $post = $repository->findOneBy(['post_name' => $postName]);
+
+        // Si no existe el post hacemos una excepcion
+        if(!$post){
+            throw $this->createNotFoundException(sprintf('No post for the name given "%s"', $postName));
+        }
+
+        // En esta url http://127.0.0.1:8000/blog/something-new-sup-yoga-with-kids
+        // var_dump($post);
+        // die;
+
+        // return $this->render('posts/post.html.twig', [
+        //     'title' => ucwords(str_replace('-',' ', $post))
+        // ]);
+
+        
+        // Ahora pasamos todo el objeto post
+        return $this->render('posts/post.html.twig', [
+            'post' => $post
+        ]);
+
     }
 }
