e3418fc116a3e218ec0d19e3ac823aa657b7a567
Dvdam <rdvdam@gmail.com>
2019-06-16 21:19:51 -0300

    postcontroller

---
+++ src/Controller/PostController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController
+{
+    /**
+     * @Route()
+     */
+    public function home()
+    {
+        return new  Response('Hello tech yogi friends');
+    }
+}
